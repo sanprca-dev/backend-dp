8f27acd1ce5adaf46c4b9b3a6ad40d762de9177d
sanprca-dev <san.prca@gmail.com>
2026-07-14 17:46:29 -0500

    Delete backend-dp directory

--- backend-dp/src/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getChatResponse } = require('../controllers/chatController');
-const { validateConsulta } = require('../middleware/validateConsulta');
-
-const router = express.Router();
-router.post('/', validateConsulta, getChatResponse);
-module.exports = router;
--- backend-dp/src/routes/expedienteRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getExpediente } = require('../controllers/expedienteController');
-const { validateConsulta } = require('../middleware/validateConsulta');
-
-const router = express.Router();
-router.post('/', validateConsulta, getExpediente);
-module.exports = router;
